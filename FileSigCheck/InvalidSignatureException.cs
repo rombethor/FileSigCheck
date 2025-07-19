d0b8edcac3b472467f743a2475e402b9af1f33d0
Daniel Thorne <thorne.danielj@gmail.com>
2025-07-18 19:03:45 -0600

    Adds `InvalidSignatureException`

---
+++ FileSigCheck/InvalidSignatureException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FileSigCheck
+{
+    /// <summary>
+    /// Invalid file signature exception.
+    /// </summary>
+    public class InvalidSignatureException : Exception
+    {
+        /// <summary>
+        /// The detected bytes from the beginning of the file
+        /// </summary>
+        public byte[] DetectedBytes { get; set; } = new byte[0];
+
+        /// <summary>
+        /// The file signatures checked against, of which this file does not match any.
+        /// </summary>
+        public string[] PermittedExtensions { get; set; } = Array.Empty<string>();
+    }
+}
